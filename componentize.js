610782b50dc42ccac5aafaf55b28624742f7afed
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2023-12-15 16:43:15 +0100

    Initial version

---
+++ componentize.js
@@ -0,0 +1,12 @@
+import { componentize } from '@bytecodealliance/componentize-js';
+import { readFile, writeFile } from 'node:fs/promises';
+import { resolve } from 'node:path';
+
+const jsSource = await readFile('main.js', 'utf8');
+
+const { component } = await componentize(jsSource, {
+  witPath: resolve('wit'),
+  debug: true
+});
+
+await writeFile('out/component.wasm', component);
